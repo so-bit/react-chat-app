918ac05f514ced96a59ae6c4e7c853b347f8d213
Sonal Bijitkar <sonalbijitkar2015@gmail.com>
2024-06-03 22:38:33 -0700

    react chat app for multiple users.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import './App.css';
+import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const auth=getAuth();
+
+  const googleLogin=()=>{
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    setUser({name:result.user.displayName, email: result.user.email})
+    console.log(token, user);
+
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+  }
+
+  const [user, setUser]=useState('');
+  const [chats, setChats]=useState([]);
+  const [msg, setMsg]=useState('');
+
+  const db = getDatabase();
+  const chatListRef = ref(db, 'chats');
+
+
+  const updateHeight=()=>{
+    const el= document.getElementById('chat');
+    if(el){
+      el.scrollTop=el.scrollHeight;
+    }
+    
+  }
+  
+  useEffect(()=>{
+    onChildAdded(chatListRef, (data) => {
+        setChats(chats=>[...chats,data.val()])
+        setTimeout(()=>{
+          updateHeight()
+        }, 100)    
+      
+    });
+  },[])
+
+  const sendChat=()=>{
+
+    const chatRef = push(chatListRef);
+    set(chatRef, {
+      user, message: msg
+    });
+    setMsg('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  <div>
+    {user.email ?  null: <div>
+      <button onClick={e=>{googleLogin()}}>Google SignIn</button>
+    </div>}  
+    
+  {user.email? <div>
+    <h3>User: {user.name}</h3>
+    <div id="chat" className="chat-container">
+      {chats.map((c, i)=> 
+      <div key={i} className={`container ${c.user.email === user.email ? 'me':''}`}>
+        <p className="chatbox">
+          <strong>{c.user.name}: </strong>
+          <span>{c.message}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div> 
+    )}    
+    </div> 
+    <div className="btm">
+      <input 
+        type="text" 
+        onInput={(e)=>setMsg(e.target.value) } 
+        value={msg}
+        placeholder="enter your chat"
+
+      ></input>
+      <button onClick={(e)=>sendChat()}>Send</button>
     </div>
+    </div>:null}
+
+    </div>
+
+ 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0wWqYXNrCqXX0bR0YV6yqT3g_Yt2Db0k",
+  authDomain: "react-chat-app-3c139.firebaseapp.com",
+  databaseURL: "https://react-chat-app-3c139-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-3c139",
+  storageBucket: "react-chat-app-3c139.appspot.com",
+  messagingSenderId: "517903695536",
+  appId: "1:517903695536:web:31cd0cd342082e035bc575"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
